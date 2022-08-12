aecc217549b63dc98ba2f28f9fb42479ad35f300
trungtashi99 <trungtashi@gmai.com>
2022-08-12 11:03:01 +0700

    Bài tập tìm giá trị lớn nhất trong mảng 2 chiều

---
+++ src/FindMaxValue.java
@@ -0,0 +1,18 @@
+public class FindMaxValue {
+        public static void main(String[] args) {
+            int arr[][] = {{1, 2}, {3, 4}, {5, 6}};
+            System.out.print(findMax(arr));
+        }
+        public static int findMax(int[][] arr){
+            int max;
+            max = arr[0][0];
+            for (int i = 0; i < arr.length; i++) {
+                for (int j = 0; j < arr[0].length; j++) {
+                    if (arr[i][j] > max){
+                        max = arr[i][j];
+                    }
+                }
+            }
+            return max;
+        }
+}
